a0d566ba75880f256a8de3371e1c7f93909d4769
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-06-30 10:29:18 +0530

    assignment-1

---
+++ block-BMaaae/index.js
@@ -0,0 +1,26 @@
+var group = [
+  {
+    name: 'user 1',
+    age: 26,
+    email: 'narendrapalacharla@gmail.com',
+    bid_price: 200,
+  },
+  {
+    name: 'user 2',
+    age: 26,
+    email: 'narendrapalacharla@gmail.com',
+    bid_price: 200,
+  },
+  {
+    name: 'user 3',
+    age: 26,
+    email: 'narendrapalacharla@gmail.com',
+    bid_price: 200,
+  },
+  {
+    name: 'user 4',
+    age: 26,
+    email: 'narendrapalacharla@gmail.com',
+    bid_price: 200,
+  },
+];
